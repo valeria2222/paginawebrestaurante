e77ae4e8576068da52ce4b707b3ce6e3ca401b5c
valeria2222 <valeriapozo2001@gmail.com>
2023-12-20 17:19:47 -0300

    Pagina Web Restaurante

---
+++ public_html/assets/js/pigga.js
@@ -0,0 +1,21 @@
+
+$('.message a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+ });
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+}); 
